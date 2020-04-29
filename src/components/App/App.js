ad1e8e7caf065f9e81e44afdfb44274bf5264a9f
Brandy Mello <brandyamello@gmail.com>
2020-04-29 11:43:01 -0600

    Add boilerplate files

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import { render } from '@testing-library/react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Recipe Roulette</h1>
+        </header>
+        <footer>
+
+        </footer>
+      </>
+    )
+  }
+}
+
+
+
+export default App;
