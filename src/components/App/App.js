d99e88f8eb8d197899c3cc67b69fceb63b83cbeb
Brandy Mello <brandyamello@gmail.com>
2020-04-29 11:57:22 -0600

    Add icons for sharing in footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  IoLogoPinterest,
+  IoLogoTwitter,
+  IoLogoFacebook
+} from "react-icons/io";
+import { MdEmail } from 'react-icons/md';
+import { FaFacebookMessenger } from 'react-icons/fa';
 // import { render } from '@testing-library/react';
 
 class App extends Component {
@@ -13,7 +20,11 @@ class App extends Component {
           <h1>Recipe Roulette</h1>
         </header>
         <footer>
-
+          <IoLogoPinterest/>
+          <IoLogoTwitter/>
+          <IoLogoFacebook/>
+          <MdEmail/>
+          <FaFacebookMessenger/>
         </footer>
       </>
     )
